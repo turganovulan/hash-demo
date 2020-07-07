d31d9c0d9f3e8bc3a295f927143d00808d5adc7a
Turganov Ulan <turganovulan@gmail.com>
2020-07-07 17:44:07 +0600

    BChain added

--- script.js
+++ script.js
@@ -1,64 +1,71 @@
-var protocol1 = {
-  text: 'P1 text'
+class Block {
+  constructor(id, data, prevHash) {
+    this.id = id;
+    this.data = Object.assign({}, data);
+    this.copy = Object.assign({}, data);
+    this.prevHash = prevHash;
+    this.hash = this.getHash();
+    
+    $(this.id).find('.p-input').on('keyup change keydown', e => {
+      this.copy.text = e.target.value;
+      this.draw()
+      this.validate()
+    })
+  }
+
+  draw () {
+    $(this.id).find('.p-text').text(this.data.text)
+    $(this.id).find('.p-hash').text(this.hash)
+    $(this.id).find('.p-input').val(this.copy.text)
+    $(this.id).find('.p-copy-text').text(this.copy.text)
+    $(this.id).find('.p-copy-hash').text(this.getHash())
+  }
+
+  setValidity(validity) {
+    $(this.id).find('.p-copy').toggleClass('border-danger', !validity)
+    $(this.id).find('.p-copy').toggleClass('border-success ', validity)
+    $(this.id).find('.p-copy .card-footer').toggleClass('bg-danger', !validity)
+    $(this.id).find('.p-copy .card-footer').toggleClass('bg-success ', validity)
+  }
+
+  getHash() {
+    var encript = JSON.stringify(this.copy) + this.prevHash;
+    return objectHash.MD5(encript);
+  }
 }
-var protocol1edit = {
-  text: 'P1 text'
-}
 
-var protocol2 = {
-  text: 'P2 text'
-}
-var protocol2edit = {
-  text: 'text'
-}
-var tmp_hash
+class BlockChain {
+  constructor() {
+    this.chain = [];
+  }
 
-function updateHash(id, protocol, edit, hash) {
-  const result = Object.assign({ hash: hash }, protocol, edit)
-  const result_hash = objectHash(result)
+  addBlock(id, data) {
+    const prevHash = this.chain.length !== 0 ? this.chain[this.chain.length - 1].hash : 0;
+    const block = new Block(id, data, prevHash);
+    block.validate = this.validate.bind(this);
+    block.draw()
+    this.chain.push(block);
+  }
 
-  $(id + ' .p-text').text(result.text)
-  $(id + ' .p-hash').text(result_hash)
-
-  const protocol_hash = objectHash(protocol)
-  const edit_hash = objectHash(Object.assign({}, protocol, edit))
-  $(id).toggleClass('border-danger', protocol_hash !== edit_hash)
-  $(id).toggleClass('border-success ', protocol_hash === edit_hash)
-  $(id + ' .card-footer').toggleClass('bg-danger', protocol_hash !== edit_hash)
-  $(id + ' .card-footer').toggleClass('bg-success ', protocol_hash === edit_hash)
-
-  return result_hash
-}
-
-function update() {
-  const hash1 = objectHash(protocol1)
-  const hash2 = objectHash(protocol2)
-
-  $('#p-1-text').text(protocol1.text)
-  $('#p-1e-input').val(protocol1edit.text)
-  $('#p-2-text').text(protocol2.text)
-  $('#p-2e-input').val(protocol2edit.text)
-
-  tmp_hash = updateHash('#p-1r1', protocol1, protocol1edit)
-  tmp_hash = updateHash('#p-1r2', protocol1, protocol1edit, tmp_hash)
-  tmp_hash = updateHash('#p-1r3', protocol1, protocol1edit, tmp_hash)
-  tmp_hash = updateHash('#p-2r1', protocol2, protocol2edit)
-  tmp_hash = updateHash('#p-2r2', protocol2, protocol2edit, tmp_hash)
-  tmp_hash = updateHash('#p-2r3', protocol2, protocol2edit, tmp_hash)
-  
-  $('#p-1-hash').text(hash1)
-  $('#p-2-hash').text(hash2)
+  validate() {
+    let validity = true;
+    for (var i = 0; i < this.chain.length; i++) {
+      if (this.chain[i].hash !== this.chain[i].getHash()) {
+        validity = false
+      }
+      if (i > 0 && this.chain[i].prevHash !== this.chain[i - 1].hash) {
+        validity = false
+      }
+      this.chain[i].setValidity(validity)
+    }
+  }
 }
 
 $(document).ready(function () {
-  $('#p-1e-input').on('keyup change keydown', e => {
-    protocol1edit.text = e.target.value;
-    update()
-  })
-  $('#p-2e-input').on('keyup change keydown', e => {
-    protocol2edit.text = e.target.value;
-    update()
-  })
+  const BChain = new BlockChain();
 
-  update()
+  BChain.addBlock('#block1', { text: 'Text1' })
+  BChain.addBlock('#block2', { text: 'Text2' })
+  BChain.addBlock('#block3', { text: 'Text3' })
+  BChain.validate()
 });
