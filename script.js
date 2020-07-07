8cc0ee5c4bc6e82f235156995239743debae6197
Turganov Ulan <turganovulan@gmail.com>
2020-07-07 20:00:11 +0600

    Traslation

--- script.js
+++ script.js
@@ -1,31 +1,87 @@
 class Block {
-  constructor(id, data, prevHash) {
-    this.id = id;
+  names = { answer: 'Ответ: ', user: 'ФИО: ' }
+
+  constructor(index, data, prevHash, validate) {
+    this.index = index;
     this.data = Object.assign({}, data);
     this.copy = Object.assign({}, data);
     this.prevHash = prevHash;
     this.hash = this.getHash();
-    
-    $(this.id).find('.p-input').on('keyup change keydown', e => {
-      this.copy.text = e.target.value;
-      this.draw()
-      this.validate()
-    })
+    this.validate = validate;
+
+    this.element = this.template()
+    this.draw()
   }
 
-  draw () {
-    $(this.id).find('.p-text').text(this.data.text)
-    $(this.id).find('.p-hash').text(this.hash)
-    $(this.id).find('.p-input').val(this.copy.text)
-    $(this.id).find('.p-copy-text').text(this.copy.text)
-    $(this.id).find('.p-copy-hash').text(this.getHash())
+  template() {
+    const name = 'Запись ' + this.index;
+    const element = $(`<div class="card-deck flex-nowrap mb-5">
+        <div class="card bg-light">
+          <div class="card-header font-weight-bold">${name}</div>
+          <div class="card-body p-data"></div>
+          <div class="card-footer text-center text-muted font-weight-bold small p-hash">></div>
+        </div>
+        <div class="card">
+          <div class="card-header">Злоумышленник</div>
+          <div class="card-body p-input"></div>
+        </div>
+        <div class="card p-copy">
+          <div class="card-header">Копия протокола</div>
+          <div class="card-body p-copy-data"></div>
+          <div class="card-footer text-center text-white font-weight-bold small p-copy-hash"></div>
+        </div>
+      </div>`, { id: 'block' + this.index })
+    $('#main').append(element)
+
+    return element
+  }
+
+  draw() {
+    Object.keys(this.data).forEach(key => {
+      const field = this.element.find('.p-data-' + key)
+      const copyField = this.element.find('.p-copy-data-' + key)
+      const input = this.element.find('.p-input-' + key)
+
+      if (field.length) {
+        field.text(this.data[key])
+        copyField.text(this.copy[key])
+      } else {
+        const item = $('<div>', { class: 'my-2' })
+        item.append($('<span>', { class: 'h6' }).text(this.names[key]))
+        item.append($('<span>', { class: 'p-data-' + key }).text(this.data[key]))
+        this.element.find('.p-data').append(item)
+        const copyItem = $('<div>', { class: 'my-2' })
+        copyItem.append($('<span>', { class: 'h6' }).text(this.names[key]))
+        copyItem.append($('<span>', { class: 'p-copy-data-' + key }).text(this.copy[key]))
+        this.element.find('.p-copy-data').append(copyItem)
+      }
+
+      if (input.length) {
+        input.val(this.copy[key])
+      } else {
+        const item = $('<div>', { class: 'd-flex align-items-center' })
+        const inputEl = $('<input>', { type: 'text', class: 'form-control form-control-sm p-input-' + key }).val(this.copy[key])
+        item.append($('<label>', { class: 'h6 col-form-label mr-2' }).text(this.names[key]))
+        item.append(inputEl)
+        this.element.find('.p-input').append(item)
+
+        inputEl.on('keyup change keydown', e => {
+          this.copy[key] = e.target.value;
+          this.draw()
+          this.validate()
+        })
+      }
+    })
+
+    this.element.find('.p-hash').text(this.hash)
+    this.element.find('.p-copy-hash').text(this.getHash())
   }
 
   setValidity(validity) {
-    $(this.id).find('.p-copy').toggleClass('border-danger', !validity)
-    $(this.id).find('.p-copy').toggleClass('border-success ', validity)
-    $(this.id).find('.p-copy .card-footer').toggleClass('bg-danger', !validity)
-    $(this.id).find('.p-copy .card-footer').toggleClass('bg-success ', validity)
+    this.element.find('.p-copy').toggleClass('border-danger', !validity)
+    this.element.find('.p-copy').toggleClass('border-success ', validity)
+    this.element.find('.p-copy .card-footer').toggleClass('bg-danger', !validity)
+    this.element.find('.p-copy .card-footer').toggleClass('bg-success ', validity)
   }
 
   getHash() {
@@ -39,12 +95,12 @@ class BlockChain {
     this.chain = [];
   }
 
-  addBlock(id, data) {
+  addBlock(data) {
+    const index = this.chain.length + 1;
     const prevHash = this.chain.length !== 0 ? this.chain[this.chain.length - 1].hash : 0;
-    const block = new Block(id, data, prevHash);
-    block.validate = this.validate.bind(this);
-    block.draw()
+    const block = new Block(index, data, prevHash, this.validate.bind(this));
     this.chain.push(block);
+    this.validate()
   }
 
   validate() {
@@ -64,8 +120,7 @@ class BlockChain {
 $(document).ready(function () {
   const BChain = new BlockChain();
 
-  BChain.addBlock('#block1', { text: 'Text1' })
-  BChain.addBlock('#block2', { text: 'Text2' })
-  BChain.addBlock('#block3', { text: 'Text3' })
-  BChain.validate()
+  BChain.addBlock({ user: 'Светлана Анатольевна', answer: 'Да' })
+  BChain.addBlock({ user: 'Олег Владимирович', answer: 'Воздержался' })
+  BChain.addBlock({ user: 'Виктор Григорьевич', answer: 'Нет' })
 });
